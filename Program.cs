6c23122686b8ff9f9bf9d72ccb6ab9e33a91582a
Donald189 <86645506+Donald189@users.noreply.github.com>
2022-02-28 20:10:57 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,24 +23,24 @@ namespace EmployeeWageMultipleCompany
             //EmpMonthlyWageCalc empMonthlyWageCalc = new EmpMonthlyWageCalc();
             //empMonthlyWageCalc.EmpWageCalcWithHoursOrDays();
 
-            ////For multiple company
-            //MultipleCompanyWage EmployWageobj = new MultipleCompanyWage();
-            //Console.WriteLine("Company 1: " + EmployWageobj.Company1);
-            //EmployWageobj.CalEmpWage();
-            //MultipleCompanyWage2 multipleCompanyWage2 = new MultipleCompanyWage2();
-            //Console.WriteLine("Company 2: " + multipleCompanyWage2.Company2);
-            //multipleCompanyWage2.CalEmpWage();
-            //MultipleCompanyWage3 multipleCompanyWage3 = new MultipleCompanyWage3();
-            //Console.WriteLine("Company 3: " + multipleCompanyWage3.Company3);
-            //multipleCompanyWage3.CalEmpWage();
+            //For multiple company
+            MultipleCompanyWage EmployWageobj = new MultipleCompanyWage();
+            Console.WriteLine("Company 1: " + EmployWageobj.Company1);
+            EmployWageobj.CalEmpWage();
+            MultipleCompanyWage2 multipleCompanyWage2 = new MultipleCompanyWage2();
+            Console.WriteLine("Company 2: " + multipleCompanyWage2.Company2);
+            multipleCompanyWage2.CalEmpWage();
+            MultipleCompanyWage3 multipleCompanyWage3 = new MultipleCompanyWage3();
+            Console.WriteLine("Company 3: " + multipleCompanyWage3.Company3);
+            multipleCompanyWage3.CalEmpWage();
 
-            //For Employee Wage Manage
-            EmployeeWageManage obj = new EmployeeWageManage("Dmart", 20, 2, 10);
-            EmployeeWageManage relaince = new EmployeeWageManage("Relaince", 10, 4, 20);
-            obj.computeEmpWage();
-            Console.WriteLine(obj.toString());
-            relaince.computeEmpWage();
-            Console.WriteLine(relaince.toString());
+            ////For Employee Wage Manage
+            //EmployeeWageManage obj = new EmployeeWageManage("Dmart", 20, 2, 10);
+            //EmployeeWageManage relaince = new EmployeeWageManage("Relaince", 10, 4, 20);
+            //obj.computeEmpWage();
+            //Console.WriteLine(obj.toString());
+            //relaince.computeEmpWage();
+            //Console.WriteLine(relaince.toString());
         }
     }
 }
